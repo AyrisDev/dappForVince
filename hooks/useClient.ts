5227231c683f9d6d9b622f9f0fe3f7094cde98f7
Mustafa YILDIZ <ayris.dev@gmail.com>
2023-07-08 04:36:55 +0300

    swap

--- hooks/useClient.ts
+++ hooks/useClient.ts
@@ -2,7 +2,7 @@ import { osmosis } from "osmojs";
 import { useChain } from "@cosmos-kit/react";
 
 export const useClient = (chainName: string) => {
-  const { getRpcEndpoint } = "http://154.53.47.14:26657/";
+  const { getRpcEndpoint } = useChain(chainName);
 
   const getClient = async () => {
     let rpcEndpoint = await getRpcEndpoint();
